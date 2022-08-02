d46a539e2e2f9d5306058fb13903fef8bb677004
tizee <33030965+tizee@users.noreply.github.com>
2022-08-02 18:32:41 +0800

    update bench for range

--- README.md
+++ README.md
@@ -40,6 +40,46 @@ it.range(u32,0,100,1)
 
 ## Performance
 
-```
+- Range
 
+```
+  times: 40
+  while  :       1000ns  base
+  context:       4000ns ~3.997002997002997e+00 times
+  range  :       1000ns ~1.0e+00 times
+
+
+
+  times: 400
+  while  :       2000ns  base
+  context:       3000ns ~1.4997501249375311e+00 times
+  range  :       3000ns ~1.4997501249375311e+00 times
+
+
+
+  times: 4000
+  while  :       14000ns  base
+  context:       32000ns ~2.28562245553889e+00 times
+  range  :       31000ns ~2.2141989857867297e+00 times
+
+
+
+  times: 40000
+  while  :       144000ns  base
+  context:       311000ns ~2.1597141686516066e+00 times
+  range  :       310000ns ~2.1527697724321357e+00 times
+
+
+
+  times: 400000
+  while  :       1437000ns  base
+  context:       3262000ns ~2.2700060751523488e+00 times
+  range  :       3098000ns ~2.1558795018235895e+00 times
+
+
+
+  times: 4000000
+  while  :       14478000ns  base
+  context:       31345000ns ~2.165008898673235e+00 times
+  range  :       31183000ns ~2.1538195086462557e+00 times
 ```
--- bench/range_bench.zig
+++ bench/range_bench.zig
@@ -4,56 +4,69 @@ const RangeContext = @import("../src/range.zig").RangeContext;
 const range = @import("../src/range.zig").range;
 const Range64 = RangeContext(u64);
 
-const TIMES: u64 = 1_000_000;
+const MAX: u64 = 5_000_000;
 
-fn whileLoop() void {
+fn whileLoop(times: u64) void {
     var i: u64 = 0;
-    while (i < TIMES) : (i += 1) {
+    while (i < times) : (i += 1) {
         asm volatile ("" ::: "memory");
     }
 }
 
-fn rangeLoop() void {
-    var iter = range(u64, 0, TIMES, 1);
+fn rangeLoop(times: u64) void {
+    var iter = range(u64, 0, times, 1);
 
     while (iter.next()) |_| {
         asm volatile ("" ::: "memory");
     }
 }
 
-fn rangeContextLoop() void {
-    var iter = Range64.init(0, TIMES, 1);
+fn rangeContextLoop(times: u64) void {
+    var iter = Range64.init(0, times, 1);
 
     while (iter.nextFn()) |_| {
         asm volatile ("" ::: "memory");
     }
 }
 
+fn bench() void {
+    var i: u64 = 40;
+    while (i < MAX) : (i *= 10) {
+        var start = std.time.nanoTimestamp();
+        whileLoop(i);
+        var end = std.time.nanoTimestamp();
+        const whileTime = end - start;
+
+        start = std.time.nanoTimestamp();
+        rangeLoop(i);
+        end = std.time.nanoTimestamp();
+        const contextTime = end - start;
+
+        start = std.time.nanoTimestamp();
+        rangeLoop(i);
+        end = std.time.nanoTimestamp();
+        const rangeTime = end - start;
+
+        debug.print(
+            \\
+            \\
+            \\  times: {}
+            \\  while  :       {}ns  base
+            \\  context:       {}ns ~{} times
+            \\  range  :       {}ns ~{} times
+            \\
+            \\
+        , .{
+            i,
+            whileTime,
+            contextTime,
+            @intToFloat(f64, contextTime + 1) / @intToFloat(f64, whileTime + 1),
+            rangeTime,
+            @intToFloat(f64, rangeTime + 1) / @intToFloat(f64, whileTime + 1),
+        });
+    }
+}
+
 pub fn main() anyerror!void {
-    var start = std.time.nanoTimestamp();
-    whileLoop();
-    var end = std.time.nanoTimestamp();
-    const whileTime = end - start;
-
-    start = std.time.nanoTimestamp();
-    rangeLoop();
-    end = std.time.nanoTimestamp();
-    const contextTime = end - start;
-
-    start = std.time.nanoTimestamp();
-    rangeLoop();
-    end = std.time.nanoTimestamp();
-    const rangeTime = end - start;
-
-    debug.print(
-        \\  while  :       {}ns  base
-        \\  context:       {}ns {} times
-        \\  range  :       {}ns {} times
-    , .{
-        whileTime,
-        contextTime,
-        @divFloor(contextTime, whileTime),
-        rangeTime,
-        @divFloor(rangeTime, whileTime),
-    });
+    bench();
 }
